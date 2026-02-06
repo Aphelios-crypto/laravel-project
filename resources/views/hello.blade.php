0db1ad8a0a48366abf4925f45b808eb589481627
Aphelios-crypto <palmajg.589.stud@cdd.edu.ph>
2026-02-06 15:46:24 +0800

    Hello Arvin

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First Blade</title>
+</head>
+<body>
+    <h1>Hello World From Team 4s</h1>
+    <p>This is my first Laravel Blade view.</p>
+</body>
+</html>
